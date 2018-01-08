2ade08cd60ef7f93fa6428b136f912ad89b69bbc
Dalton Fant <daltonfant16@gmail.com>
2018-01-08 16:58:16 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,9 +11,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).odd?
-    return "O"
-  else
+  if turn_count(board).even?
     return "X"
+  else
+    return "O"
   end
 end
