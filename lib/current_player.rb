f6350d3d1822a56549fc7dd94dfe2692485a2e6c
Dalton Fant <daltonfant16@gmail.com>
2018-01-08 16:47:14 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+      return turns
+    end
+  end
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    return "X"
+  else
+    return "O"
+  end
+end
